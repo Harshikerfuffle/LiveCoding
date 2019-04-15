e3f858d003321baeebc6b2005846ba99f5a11167
Harshikerfuffle <harshika30j@gmail.com>
2019-04-15 16:40:14 -0400

    Update and rename algorave2.ino to algorave.ino

--- Triangles/algorave.ino
+++ Triangles/algorave.ino
@@ -1,6 +1,9 @@
+//FOR ARDUINO CONSOLE 
+//code has been referenced from multiple locations
+
 int ledPin = 7;
-int potPin = A0;
-int photoPin = A1;
+int potPin = A0; //initialise potentiometer
+int photoPin = A1; //initialise photosensor
 int potOutput;
 int photoReading;
 
@@ -14,13 +17,17 @@ void loop() {
   photoReading = analogRead (photoPin);
 //  Serial.println(photoReading);
   
-//  int mappedOutput = map(potOutput, 0, 1023, 0, 255); //map the output
-  int mappedOutputB = map(photoReading, 0, 1023, 0, 255);
+//  only one mappedOutput should be used at a time otherwise the processing console gets confused since it 
+//   receives different values from 2 sensors at the same time
+  
+//   int mappedOutput = map(potOutput, 0, 1023, 0, 255); //map the potentiometer output
+  int mappedOutputB = map(photoReading, 0, 1023, 0, 255); //map photosensor output
 
 //  Serial.println(mappedOutput);
   Serial.println(mappedOutputB);
   
-//  Serial.write(mappedOutput);
+  
+//  Serial.write(mappedOutput); //for sending signals to processing to pick 
 
   if (Serial.available ( ) > 0) { // Checking if the Processing IDE has send a value or not
     char state = Serial.read ( ); // Reading the data received and saving in the state variable
